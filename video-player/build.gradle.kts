e537d561db566d2a3865fc24915f9a97b18f7fcc
Lanlinju <lanlinju@outlook.com>
2025-06-09 16:24:41 +0800

    chore: 升级相关依赖项

--- video-player/build.gradle.kts
+++ video-player/build.gradle.kts
@@ -37,13 +37,12 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-compose:1.9.2")
-    implementation("androidx.compose.ui:ui:1.8.2")
-    implementation("androidx.compose.material3:material3:1.3.0")
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.material3)
+    implementation(platform(libs.androidx.compose.bom))
 //    api("com.google.android.exoplayer:exoplayer-core:2.19.1")
 //    api("com.google.android.exoplayer:exoplayer-hls:2.19.1")
-
-    val media3 = "1.7.1"
-    api("androidx.media3:media3-exoplayer:$media3")
-    api("androidx.media3:media3-exoplayer-hls:$media3")
+    api(libs.androidx.media3.exoplayer)
+    api(libs.androidx.media3.exoplayer.hls)
 }

064b3d38f0a047b2a6578ee26e1617a138bf1295
Lanlinju <lanlinju@outlook.com>
2025-06-30 16:12:21 +0800

    chore: Add Foojay toolchain resolver plugin to Gradle settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,17 @@
 pluginManagement {
     repositories {
-        maven("https://maven.aliyun.com/repository/public")
-        maven("https://jitpack.io")
+//        maven("https://maven.aliyun.com/repository/public")
+//        maven("https://jitpack.io")
         google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version ("1.0.0")
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {

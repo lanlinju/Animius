f8f44e5c7d515ccfe6515c52b49f37e4819b904a
Lanlinju <lanlinju@outlook.com>
2025-06-30 14:52:02 +0800

    chore: Remove unused Maven repository URLs from Gradle settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,7 @@
 pluginManagement {
     repositories {
-        maven { setUrl("https://maven.aliyun.com/repository/central") }
-        maven { setUrl("https://maven.aliyun.com/repository/jcenter") }
-        maven { setUrl("https://maven.aliyun.com/repository/google") }
-        maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
-        maven { setUrl("https://maven.aliyun.com/repository/public") }
-        maven { setUrl("https://jitpack.io") }
+        maven("https://maven.aliyun.com/repository/public")
+        maven("https://jitpack.io")
         google()
         mavenCentral()
         gradlePluginPortal()
@@ -14,12 +10,8 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { setUrl("https://maven.aliyun.com/repository/central") }
-        maven { setUrl("https://maven.aliyun.com/repository/jcenter") }
-        maven { setUrl("https://maven.aliyun.com/repository/google") }
-        maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
-        maven { setUrl("https://maven.aliyun.com/repository/public") }
-        maven { setUrl("https://jitpack.io") }
+        maven("https://maven.aliyun.com/repository/public")
+        maven("https://jitpack.io")
         google()
         mavenCentral()
     }

c8209f64f6f5293acc919bdd0a40932e22ae1bc3
Lanlinju <lanlinju@outlook.com>
2025-07-01 15:50:41 +0800

    chore: 清理不必要的Maven仓库配置

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,7 @@
 pluginManagement {
     repositories {
-        maven { setUrl("https://maven.aliyun.com/repository/central") }
-        maven { setUrl("https://maven.aliyun.com/repository/jcenter") }
-        maven { setUrl("https://maven.aliyun.com/repository/google") }
-        maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
-        maven { setUrl("https://maven.aliyun.com/repository/public") }
-        maven { setUrl("https://jitpack.io") }
+//        maven { setUrl("https://maven.aliyun.com/repository/public") }
+//        maven { setUrl("https://jitpack.io") }
         google()
         mavenCentral()
         gradlePluginPortal()
@@ -14,10 +10,6 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { setUrl("https://maven.aliyun.com/repository/central") }
-        maven { setUrl("https://maven.aliyun.com/repository/jcenter") }
-        maven { setUrl("https://maven.aliyun.com/repository/google") }
-        maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
         maven { setUrl("https://maven.aliyun.com/repository/public") }
         maven { setUrl("https://jitpack.io") }
         google()
